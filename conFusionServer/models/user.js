a74a59c4bdd8972619edcb07a2bbf1d4b42b05ec
Parth Parmar <parthparmar15@gmail.com>
2019-04-02 12:03:20 -0400

    Mongoose population

--- conFusionServer/models/user.js
+++ conFusionServer/models/user.js
@@ -5,6 +5,15 @@ var passportLocalMongoose = require('passport-local-mongoose');
 
 var User = new Schema({
     
+    firstname: {
+        type: String,
+          default: ''
+      },
+      lastname: {
+        type: String,
+          default: ''
+      },
+    
     admin:   {
         type: Boolean,
         default: false

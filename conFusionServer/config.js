b7ef97350557b4fb7eb56eccb1381ed72d565283
parthkumar parmar <parth.parmar@mail.utoronto.ca>
2019-05-17 15:57:03 -0400

    Updated index.html

--- conFusionServer/config.js
+++ conFusionServer/config.js
@@ -1,8 +1,5 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
     'mongoUrl' : 'mongodb://localhost:27017/conFusion',
-    'facebook':{
-        clientId: '2188365877914700',
-        clientSecret: '6184a7e7f46c3cc61c8fb2a3c4f5e04d'
-    }
+    
 }
